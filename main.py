ada04a168341ad75765450f20e5f0ae4b20ba145
Ititbaba <155376701+Ititbaba@users.noreply.github.com>
2026-07-17 08:15:26 +0300

    add config use to main

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import asyncio
 
+from config import settings
 from models.binance_client import BinanceClient
 from models.monitoring import Monitor
 from models.order_book import OrderBook
 from models.sync_manager import SyncManager
 
-SYMBOL = "btcusdt"
-
 
 async def main() -> None:
-    binance_client = BinanceClient(symbol=SYMBOL)
-    order_book = OrderBook(symbol=SYMBOL)
+    binance_client = BinanceClient(symbol=settings.symbol)
+    order_book = OrderBook(symbol=settings.symbol)
     monitor = Monitor()
-    sync_manager = SyncManager(symbol=SYMBOL, binance_client=binance_client, order_book=order_book, monitor=monitor)
+    sync_manager = SyncManager(
+        symbol=settings.symbol, binance_client=binance_client, order_book=order_book, monitor=monitor
+    )
 
     await sync_manager.run()
 
